01d4e65a4b5e1895e7ba6e4990f1a4e2d3e52df3
MartinSimeonovvv <marti_191997@abv.bg>
2022-01-07 18:11:59 +0200

    Creating Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,23 @@
-import React, { useState } from 'react';
-import Header from './components/Header';
-import NotesList from './components/NotesList';
-import CreateNotes from './components/CreateNotes';
-import { Note } from './models/note.model';
-import './App.css';
-import { Col, Container, Row } from 'react-bootstrap';
+import React, { useState } from "react";
+
+import { Note } from "./models/note.model";
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  const [notes, setNotes] = useState<Note[]>([{
-    id: (new Date).toString(),
-    title: "Meetings",
-    text: " Schedule meeting with UI/UX Team",
-    color: "#dfdfdf",
-    date: (new Date).toString()
-  }]);
-
+  const [notes, setNotes] = useState<Note[]>([
+    {
+      id: new Date().toString(),
+      title: "Meetings",
+      text: "Schedule meeting with UI/UX team",
+      color: "#dfdfdf",
+      date: new Date().toString(),
+    },
+  ]);
 
   return (
     <>
       <Header />
-      <Container className="mt-5">
-        <Row>
-          <Col>
-            <NotesList notes={notes} setNotes={ setNotes}/>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <CreateNotes notes={notes} setNotes={ setNotes}/>
-          </Col>
-        </Row>
-      </Container>
     </>
   );
 }
